cd5c740c252a21d5e71e27acf1c011368f33a658
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-09-09 00:31:26 +0300

    feat: plot widget

--- ai-1/src/main.rs
+++ ai-1/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+mod simulated_annealing;
+
 use eframe::egui;
 use eframe::egui::ColorImage;
 use egui::plot::{Legend, Line, Plot, PlotPoints};
@@ -18,76 +20,114 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
+static ASCII_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+pub struct CustomPlot {
+    pub plot_id: String,
+    pub data: Vec<[f64; 2]>,
+    pub width: f32,
+    pub height: f32,
+    pub title: String,
+}
+
+impl CustomPlot {
+    fn new(plot_id: impl Into<String>) -> Self {
+        Self {
+            plot_id: plot_id.into(),
+            ..Default::default()
+        }
+    }
+    // TODO: remove default and implement "with" methods
+}
+
+impl Default for CustomPlot {
+    fn default() -> Self {
+        Self {
+            plot_id: "test_plot_id".into(),
+            data: vec![[0.0, 0.0]],
+            width: 200.0,
+            height: 200.0,
+            title: "Test_title".into()
+        }
+    }
+}
+
+impl egui::Widget for CustomPlot {
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+    ui.vertical(|ui| {
+            ui.label(self.title);
+            let my_plot = Plot::new(self.plot_id)
+            .height(self.height)
+            .width(self.width)
+            .legend(Legend::default());
+
+            // let's create a dummy line in the plot
+            let graph: Vec<[f64; 2]> = vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0]];
+            my_plot.show(ui, |plot_ui| {
+                plot_ui.line(Line::new(PlotPoints::from(graph)).name("curve"));
+            })
+        }).response
+    }
+}
+
 #[derive(Default)]
 struct MyApp {
     screenshot: Option<ColorImage>,
+    max_temperature_str: String,
+    min_temperature_str: String,
+    temperature_alpha: String,
+    queens_amount: String,
+    steps_n: String,
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let mut plot_rect = None;
         egui::CentralPanel::default().show(ctx, |ui| {
             // these are just some dummy variables for the example,
             // such that the plot is not at position (0,0)
-            let height = 200.0;
+            let height = 400.0;
             let border_x = 11.0;
             let border_y = 18.0;
-            let width = 300.0;
+            let width = 600.0;
 
-            ui.heading("My egui Application");
-
+            ui.heading("Simulated annealing");
             // add some whitespace in y direction
             ui.add_space(border_y);
 
-            if ui.button("Save Plot").clicked() {
-                frame.request_screenshot();
-            }
+            ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                ui.vertical(|ui| {
+                    ui.label("Минимальная температура");
+                    ui.text_edit_singleline(&mut self.max_temperature_str);
 
-            // add some whitespace in y direction
-            ui.add_space(border_y);
+                    ui.label("Максимальная температура");
+                    ui.text_edit_singleline(&mut self.min_temperature_str);
 
-            ui.horizontal(|ui| {
-                // add some whitespace in x direction
-                ui.add_space(border_x);
+                    ui.label("Коэффициент температуры");
+                    ui.text_edit_singleline(&mut self.temperature_alpha);
 
-                let my_plot = Plot::new("My Plot")
-                    .height(height)
-                    .width(width)
-                    .legend(Legend::default());
+                    ui.label("Количество ферзей");
+                    ui.text_edit_singleline(&mut self.queens_amount);
 
-                // let's create a dummy line in the plot
-                let graph: Vec<[f64; 2]> = vec![[0.0, 1.0], [2.0, 3.0], [3.0, 2.0]];
-                let inner = my_plot.show(ui, |plot_ui| {
-                    plot_ui.line(Line::new(PlotPoints::from(graph)).name("curve"));
+                    ui.label("Количество шагов при постоянном значении температуры");
+                    ui.text_edit_singleline(&mut self.steps_n);
+
+                    if ui.button("Посчитать").clicked() {
+                        frame.request_screenshot();
+                    }
                 });
-                // Remember the position of the plot
-                plot_rect = Some(inner.response.rect);
+                egui::Grid::new("grid_plots").show(ui, |ui| {
+                    ui.add(CustomPlot::default());
+
+                    // CustomPlot::default();
+
+                    // ui.end_row();
+
+                    // CustomPlot::default();
+
+                    // CustomPlot::default();
+                });
             });
-
-            // add some whitespace in y direction
-            ui.add_space(border_y);
         });
-
-        if let (Some(screenshot), Some(plot_location)) = (self.screenshot.take(), plot_rect) {
-            if let Some(mut path) = rfd::FileDialog::new().save_file() {
-                path.set_extension("png");
-
-                // for a full size application, we should put this in a different thread,
-                // so that the GUI doesn't lag during saving
-
-                let pixels_per_point = frame.info().native_pixels_per_point;
-                let plot = screenshot.region(&plot_location, pixels_per_point);
-                // save the plot to png
-                image::save_buffer(
-                    &path,
-                    plot.as_raw(),
-                    plot.width() as u32,
-                    plot.height() as u32,
-                    image::ColorType::Rgba8,
-                )
-                .unwrap();
-            }
-        }
     }
 
     fn post_rendering(&mut self, _screen_size_px: [u32; 2], frame: &eframe::Frame) {
