4c10cebab7a9cc0bcba675a6c3075340b7af6a11
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-09-20 00:35:44 +0300

    fix/feat: fixed ai-2 colors issue+started ai-3

---
+++ ai-3/src/art1.rs
@@ -0,0 +1,173 @@
+use rand::distributions::{Alphanumeric, DistString};
+
+use bit_vec;
+
+#[derive(Debug)]
+pub struct Claster {
+    pub v: bit_vec::BitVec,
+    pub indexes: Vec<usize>,
+    pub id: String,
+}
+
+impl Claster {
+    pub fn new() -> Self {
+        Self {
+            v: bit_vec::BitVec::default(),
+            indexes: Vec::default(),
+            id: Alphanumeric.sample_string(&mut rand::thread_rng(), 16),
+        }
+    }
+    fn add(&mut self, entity: &mut ClasterEntity) {
+        if self.indexes.is_empty() {
+            self.v = entity.v.clone();
+        } else {
+            self.v.and(&entity.v);
+        }
+        self.indexes.push(entity.data_index);
+        entity.claster_id = self.id.clone();
+    }
+    pub fn recalculate(&mut self, data: &Vec<bit_vec::BitVec>) {
+        self.v.clear();
+        match self.indexes.first() {
+            Some(i) => self.v = data[*i].clone(),
+            None => (),
+        }
+        for i in &self.indexes {
+            self.v.and(&data[*i]);
+        }
+    }
+    pub fn contains(&self, index: &usize) -> bool {
+        self.indexes.contains(index)
+    }
+    pub fn remove(&mut self, data: &Vec<bit_vec::BitVec>, entity: &mut ClasterEntity) {
+        self.indexes.remove(
+            self.indexes
+                .iter()
+                .position(|x| x == &entity.data_index)
+                .unwrap(),
+        );
+        self.recalculate(data);
+        entity.claster_id = "-".into();
+    }
+}
+
+fn get_energy(v: &bit_vec::BitVec) -> u32 {
+    v.blocks().map(|x| x.count_ones()).sum()
+}
+
+fn similar(P: &bit_vec::BitVec, E: &bit_vec::BitVec, b: f64) -> bool {
+    let mut intersect = P.clone();
+    intersect.and(E);
+    get_energy(&intersect) as f64 / (b + get_energy(P) as f64)
+        > get_energy(E) as f64 / (b + P.len() as f64)
+}
+
+fn pass(P: &bit_vec::BitVec, E: &bit_vec::BitVec, p: f64) -> bool {
+    let mut intersect = P.clone();
+    intersect.and(E);
+    (get_energy(&intersect) as f64 / get_energy(E) as f64) >= p
+}
+
+#[derive(Debug)]
+struct ClasterEntity {
+    v: bit_vec::BitVec,
+    data_index: usize,
+    claster_id: String,
+}
+
+#[derive(Debug)]
+struct ClastersManager {
+    clasters: Vec<Claster>,
+    b: f64,
+    p: f64,
+}
+
+impl ClastersManager {
+    fn add_claster(&mut self, claster: Claster) {
+        self.clasters.push(claster);
+    }
+    fn remove_from_claster(&mut self, data: &Vec<bit_vec::BitVec>, entity: &mut ClasterEntity) {
+        match self.clasters.iter().position(|x| x.id == entity.claster_id) {
+            Some(index) => self.clasters[index].remove(data, entity),
+            None => (),
+        }
+    }
+    fn find_claster(&mut self, entity: &mut ClasterEntity) -> Option<usize> {
+        for i in 0..self.clasters.len() {
+            if entity.claster_id != self.clasters[i].id
+                && similar(&self.clasters[i].v, &entity.v, self.b)
+            {
+                if pass(&self.clasters[i].v, &entity.v, self.p) {
+                    return Some(i);
+                }
+            }
+        }
+        None
+    }
+    fn dispatch_entity(&mut self, data: &Vec<bit_vec::BitVec>, entity: &mut ClasterEntity) -> bool {
+        match self.find_claster(entity) {
+            Some(index) => {
+                self.remove_from_claster(data, entity);
+                self.clasters[index].add(entity);
+                true
+            }
+            None => false,
+        }
+    }
+    fn recalculate(&mut self) {
+        self.clasters = self
+            .clasters
+            .drain(..)
+            .filter(|x| x.indexes.len() != 0)
+            .collect();
+    }
+}
+
+pub fn art1(
+    data: &Vec<bit_vec::BitVec>,
+    amount_clasters: &usize,
+    p: &f64,
+    b: &f64,
+) -> (Vec<Claster>, Vec<usize>) {
+    let mut entities: Vec<_> = data
+        .iter()
+        .enumerate()
+        .map(|(i, x)| ClasterEntity {
+            v: x.clone(),
+            data_index: i,
+            claster_id: "-".into(),
+        })
+        .collect();
+    let mut first_claster = Claster::new();
+    first_claster.add(&mut entities[0]);
+    let mut clasters = ClastersManager {
+        clasters: vec![first_claster],
+        b: *b,
+        p: *p,
+    };
+
+    let mut changed = true;
+    let mut max_iters = 500;
+    while changed && max_iters > 0 {
+        max_iters -= 1;
+        changed = false;
+        for entity in &mut entities {
+            changed = clasters.dispatch_entity(data, entity);
+            if &clasters.clasters.len() < amount_clasters && entity.claster_id == "-" {
+                let mut claster = Claster::new();
+                claster.add(entity);
+                clasters.add_claster(claster);
+                changed = true;
+            }
+            clasters.recalculate();
+        }
+    }
+    (
+        clasters.clasters,
+        entities
+            .into_iter()
+            .filter(|x| x.claster_id == "-")
+            .map(|x| x.data_index)
+            .collect(),
+    )
+}
---
+++ ai-3/src/main.rs
@@ -0,0 +1,291 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // Скрывает консоль на Windows
+
+mod art1;
+
+use art1::Claster;
+use eframe::egui;
+use egui::plot::{Legend, Line, Plot, PlotPoints};
+use rand::Rng;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Лог в stderr (`RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(350.0, 400.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Simulated annealing",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
+}
+struct MyApp {
+    // Количество векторов-прототипов
+    amount_clasters: String,
+    // Параметр внимательности
+    p: String,
+    // Бета параметр
+    b: String,
+    // Кластеры
+    clasters: Option<Vec<Claster>>,
+    // Данные
+    data: Vec<bit_vec::BitVec>,
+    // Colors
+    colors: Vec<egui::Color32>,
+    // Color map
+    colors_map: Option<std::collections::HashMap<String, egui::Color32>>,
+    // Dropped
+    dropped: Vec<usize>,
+    // features
+    features: String,
+    // amount_of_data
+    amount_of_data: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        let mut data: Vec<bit_vec::BitVec> = vec![];
+        for i in 0..10 {
+            data.push(bit_vec::BitVec::from_bytes(&[rand::random::<u8>()]))
+        }
+        Self {
+            amount_clasters: "5".into(),
+            b: "1.0".into(),
+            p: "0.1".into(),
+            clasters: None,
+            data: data,
+            colors: vec![
+                egui::Color32::from_rgb(128, 0, 0),
+                egui::Color32::from_rgb(255, 99, 71),
+                egui::Color32::from_rgb(240, 128, 128),
+                egui::Color32::from_rgb(255, 165, 0),
+                egui::Color32::from_rgb(240, 230, 140),
+                egui::Color32::from_rgb(154, 205, 50),
+                egui::Color32::from_rgb(124, 252, 0),
+                egui::Color32::from_rgb(0, 250, 154),
+                egui::Color32::from_rgb(32, 178, 170),
+                egui::Color32::from_rgb(70, 130, 180),
+                egui::Color32::from_rgb(100, 149, 237),
+                egui::Color32::from_rgb(138, 43, 226),
+                egui::Color32::from_rgb(123, 104, 238),
+                egui::Color32::from_rgb(221, 160, 221),
+                egui::Color32::from_rgb(255, 248, 220),
+            ],
+            colors_map: None,
+            dropped: vec![],
+            amount_of_data: "10".into(),
+            features: "8".into(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("ART1");
+            ui.add_space(18.0);
+
+            egui::ScrollArea::new([true, true])
+                .auto_shrink([true, true])
+                .show(ui, |ui| {
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                        ui.vertical(|ui| {
+                            ui.label("Количество векторов-прототипов");
+                            ui.text_edit_singleline(&mut self.amount_clasters);
+
+                            ui.label("Параметр внимательности");
+                            ui.text_edit_singleline(&mut self.p);
+
+                            ui.label("Бета параметр");
+                            ui.text_edit_singleline(&mut self.b);
+
+                            if ui.button("Посчитать").clicked() {
+                                let p = match self.p.parse() {
+                                    Ok(v) => v,
+                                    Err(_) => {
+                                        self.p = "0.1".into();
+                                        0.1f64
+                                    }
+                                };
+                                let b = match self.b.parse() {
+                                    Ok(v) => v,
+                                    Err(_) => {
+                                        self.b = "1.0".into();
+                                        1.0f64
+                                    }
+                                };
+                                let amount_clasters = match self.amount_clasters.parse() {
+                                    Ok(v) => v,
+                                    Err(_) => {
+                                        self.amount_clasters = "5".into();
+                                        5usize
+                                    }
+                                };
+                                let (clasters, dropped) =
+                                    art1::art1(&self.data, &amount_clasters, &p, &b);
+                                self.clasters = Some(clasters);
+                                self.dropped = dropped;
+                                if self.clasters.as_ref().unwrap().len() > self.colors.len() {
+                                    self.colors_map = None;
+                                } else {
+                                    self.colors_map = Some(std::collections::HashMap::default());
+                                    for i in 0..self.clasters.as_ref().unwrap().len() {
+                                        self.colors_map.as_mut().unwrap().insert(
+                                            self.clasters.as_ref().unwrap()[i].id.clone(),
+                                            self.colors[i],
+                                        );
+                                    }
+                                }
+                            }
+                            ui.label("Количество фитч");
+                            ui.text_edit_singleline(&mut self.features);
+
+                            ui.label("Количество элементов");
+                            ui.text_edit_singleline(&mut self.amount_of_data);
+
+                            if ui.button("Сгенерировать новые данные").clicked()
+                            {
+                                self.colors_map = None;
+                                self.clasters = None;
+                                self.dropped.clear();
+                                let amount_of_data = match self.amount_of_data.parse() {
+                                    Ok(v) => v,
+                                    Err(_) => {
+                                        self.amount_of_data = "10".into();
+                                        10usize
+                                    }
+                                };
+                                let features = match self.features.parse() {
+                                    Ok(v) => v,
+                                    Err(_) => {
+                                        self.features = "8".into();
+                                        8usize
+                                    }
+                                };
+                                self.data.clear();
+                                for i in 0..amount_of_data {
+                                    self.data.push(bit_vec::BitVec::from_elem(features, false));
+                                    for j in 0..features {
+                                        if rand::random::<bool>() {
+                                            self.data[i].set(j, true);
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                        egui::ScrollArea::new([true, true])
+                            .auto_shrink([true, true])
+                            .show(ui, |ui| {
+                                egui::Grid::new("start_grid")
+                                    .min_row_height(12.0)
+                                    .min_col_width(12.0)
+                                    .spacing([6.0, 0.0])
+                                    .show(ui, |ui| {
+                                        ui.label("Изначальные векторы-признаки");
+                                        ui.end_row();
+                                        for v in &self.data {
+                                            ui.label(
+                                                egui::RichText::new(format!("{:?}", v))
+                                                    .font(egui::FontId::proportional(20.0)),
+                                            );
+                                            ui.end_row();
+                                        }
+                                    });
+                                egui::Grid::new("start_grid")
+                                    .min_row_height(12.0)
+                                    .min_col_width(12.0)
+                                    .spacing([6.0, 0.0])
+                                    .show(ui, |ui| {
+                                        ui.label("Конечные векторы-признаки");
+                                        ui.end_row();
+                                        match self.colors_map.as_ref() {
+                                            Some(color_map) => {
+                                                for claster in self.clasters.as_ref().unwrap() {
+                                                    ui.label(
+                                                        egui::RichText::new(format!(
+                                                            "Прототип: {:?}",
+                                                            claster.v
+                                                        ))
+                                                        .font(egui::FontId::proportional(25.0))
+                                                        .color(color_map[&claster.id])
+                                                        .strong(),
+                                                    );
+                                                    ui.end_row();
+                                                    for index in &claster.indexes {
+                                                        ui.label(
+                                                            egui::RichText::new(format!(
+                                                                "{:?}",
+                                                                self.data[*index]
+                                                            ))
+                                                            .font(egui::FontId::proportional(20.0))
+                                                            .color(color_map[&claster.id]),
+                                                        );
+                                                        ui.end_row();
+                                                    }
+                                                }
+                                            }
+                                            None => match self.clasters.as_ref() {
+                                                Some(clasters) => {
+                                                    for claster in self.clasters.as_ref().unwrap() {
+                                                        ui.label(
+                                                            egui::RichText::new(format!(
+                                                                "Прототип: {:?}",
+                                                                claster.v
+                                                            ))
+                                                            .font(egui::FontId::proportional(25.0))
+                                                            .strong(),
+                                                        );
+                                                        ui.end_row();
+                                                        for index in &claster.indexes {
+                                                            ui.label(
+                                                                egui::RichText::new(format!(
+                                                                    "{:?}",
+                                                                    self.data[*index]
+                                                                ))
+                                                                .font(egui::FontId::proportional(
+                                                                    20.0,
+                                                                )),
+                                                            );
+                                                            ui.end_row();
+                                                        }
+                                                    }
+                                                }
+                                                None => {
+                                                    for v in &self.data {
+                                                        ui.label(
+                                                            egui::RichText::new(format!("{:?}", v))
+                                                                .font(egui::FontId::proportional(
+                                                                    20.0,
+                                                                )),
+                                                        );
+                                                        ui.end_row();
+                                                    }
+                                                }
+                                            },
+                                        }
+                                        if self.dropped.len() != 0 {
+                                            ui.label(
+                                                egui::RichText::new("Не вошедшие в кластеры")
+                                                    .font(egui::FontId::proportional(25.0))
+                                                    .strong(),
+                                            );
+                                            ui.end_row();
+                                            for v in &self.dropped {
+                                                ui.label(
+                                                    egui::RichText::new(format!(
+                                                        "{:?}",
+                                                        self.data[*v]
+                                                    ))
+                                                    .font(egui::FontId::proportional(20.0)),
+                                                );
+                                                ui.end_row();
+                                            }
+                                        }
+                                    });
+                            });
+                    });
+                });
+        });
+    }
+}
